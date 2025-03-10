f0f4021f3c0a4928ba2feaa14c5178a4f693e915
isstanchev22 <isstanchev22@codingburgas.bg>
2025-03-10 15:46:12 +0200

    Add Menu and some features

---
+++ MovieProject/MovieProject/MovieProject.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<iomanip>
+#include<fstream>
+using namespace std;
+
+void ascii() {
+	ifstream f("ascii.txt");
+	if (f.is_open())
+		cout << f.rdbuf();
+}
+
+void printMenu() {
+	ascii();
+	int choice;
+	cout << "1|    Login / Register" << endl;
+	cout << "2|    View Top Movies" << endl;
+	cout << "3|    Locations     " << endl;
+	while (true) {
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			ascii();
+			break;
+		case 2:
+			ascii();
+			break;
+		case 3:
+			ascii();
+			break;
+		}
+	}
+	}
+int main() {
+	printMenu();
+	}
+
+
+
+
