613804c094bcf447b103bd8068fa84539b744c3c
isstanchev22 <isstanchev22@codingburgas.bg>
2025-04-14 15:22:20 +0300

    Seat functionality

--- MovieProject/MovieProject/include/Seat.h
+++ MovieProject/MovieProject/include/Seat.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <string>
+
+class Seat {
+public:
+    Seat(int number, std::string type);
+    int getSeatNumber() const;
+    std::string getType() const;
+    bool getIsBooked() const;
+
+    void bookSeat();
+
+    void displaySeatStatus() const;
+private:
+    int seatNumber;
+    std::string type;
+    bool isBooked;
+
+};
